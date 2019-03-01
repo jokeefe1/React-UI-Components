8e875c6c41bc56ff9b206f56993c8b61a7fb4b7e
John O'Keefe <jokeefe@weegolobal.com>
2019-02-28 19:58:49 -0500

    added moment.js to project

--- Project-I-Social-Card/social-card/src/components/SectionTop/SectionTop.js
+++ Project-I-Social-Card/social-card/src/components/SectionTop/SectionTop.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyledDiv} from './SectionTop.styles'
+import moment from 'moment'
 
 export default function SectionTop() {
+    const today = moment().format('Do MMM');
     return (
         <StyledDiv>
             <div>
                 <h3>Lambda School</h3>
-                <p>@LambdaSchool - 26 jan</p>
+                <p>@LambdaSchool - {`${today}`}</p>
             </div>
             <div>
                 Let's learn React by building simple interfaces with components.
